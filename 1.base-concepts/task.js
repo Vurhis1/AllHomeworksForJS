6170cba2d294cc80b5442a45589261bc69cf3189
Vurhis1 <denraimond@gmail.ru>
2022-12-15 22:26:17 +0300

    First home work

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,52 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+  let arr = [];  
+  let d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		arr.push();
+	} else if (d == 0) {		
+		let oneRoot = -b / (2 * a);
+			arr.push(oneRoot);
+	} else if (d > 0) {
+		let firstRoot = (-b + Math.sqrt(d))/(2 * a);
+		let secondRoot = (-b - Math.sqrt(d))/(2 * a);
+			arr.push(firstRoot, secondRoot);		  		
+	} 
+  return arr; 
 }
 
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+	if(isNaN(percent)) {
+		return false;
+	}
+
+	if(isNaN(contribution)) {
+		return false;
+	}
+
+	if(isNaN(amount)) {
+		return false;
+	}
+
+	if(isNaN(countMonths)) {
+		return false;
+	}
+
+	let bodyCredit = 0;
+
+	let newPercent = percent / 100 / 12;
+
+	if (contribution) {
+		bodyCredit = amount - contribution;
+	} else {
+		bodyCredit = amount;
+	}
+
+	let monthlyPayment = bodyCredit * (newPercent + (newPercent / (((1 + newPercent) ** countMonths) - 1)));
+	let totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
+
+	return totalAmount;
 }
