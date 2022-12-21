2d74656ab588a3764fec50aca2e918ff1ec2f695
Vurhis1 <denraimond@gmail.ru>
2022-12-21 16:32:31 +0300

    Change task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,15 +3,13 @@
 function solveEquation(a, b, c) {
   let arr = [];  
   let d = b ** 2 - 4 * a * c;
-	if (d < 0) {
-		arr.push();
-	} else if (d == 0) {		
-		let oneRoot = -b / (2 * a);
-			arr.push(oneRoot);
+	if (d == 0) {		
+	  let oneRoot = -b / (2 * a);
+	  arr.push(oneRoot);
 	} else if (d > 0) {
-		let firstRoot = (-b + Math.sqrt(d))/(2 * a);
-		let secondRoot = (-b - Math.sqrt(d))/(2 * a);
-			arr.push(firstRoot, secondRoot);		  		
+	  let firstRoot = (-b + Math.sqrt(d))/(2 * a);
+	  let secondRoot = (-b - Math.sqrt(d))/(2 * a);
+	  arr.push(firstRoot, secondRoot);		  		
 	} 
   return arr; 
 }
@@ -19,34 +17,27 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-	if(isNaN(percent)) {
-		return false;
-	}
+  if(isNaN(percent)) {
+	return false;
+  }
 
-	if(isNaN(contribution)) {
-		return false;
-	}
+  if(isNaN(contribution)) {
+	return false;
+  }
 
-	if(isNaN(amount)) {
-		return false;
-	}
+  if(isNaN(amount)) {
+	return false;
+  }
 
-	if(isNaN(countMonths)) {
-		return false;
-	}
+  if(isNaN(countMonths)) {
+	return false;
+  }
 
-	let bodyCredit = 0;
-
-	let newPercent = percent / 100 / 12;
-
-	if (contribution) {
-		bodyCredit = amount - contribution;
-	} else {
-		bodyCredit = amount;
-	}
-
-	let monthlyPayment = bodyCredit * (newPercent + (newPercent / (((1 + newPercent) ** countMonths) - 1)));
-	let totalAmount = Number((monthlyPayment * countMonths).toFixed(2));
-
-	return totalAmount;
-}
+  let bodyCredit = amount - contribution;
+  let newPercent = percent / 100 / 12;
+  let monthlyPayment = bodyCredit * (newPercent + (newPercent / (((1 + newPercent) ** countMonths) - 1))); 
+  let totalAmount = +Number((monthlyPayment * countMonths).toFixed(2));
+  
+  return totalAmount;
+  }
+  
