1927395251eebf3a77a968352b525389d680406f
Vurhis1 <denraimond@gmail.ru>
2023-01-04 13:19:26 +0300

    completing a task of function

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length && arr1.every((v, i) => v === arr2[i]); 
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users
+    .filter(user => user.gender === gender)
+    .map(user => user.age)
+    .reduce((summ, userAge, index, array) => userAge / array.length + summ, 0)
 }
