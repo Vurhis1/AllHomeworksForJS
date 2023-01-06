f81b9d317757259ed58049951cdd12b475dad916
Vurhis1 <denraimond@gmail.ru>
2023-01-06 13:25:44 +0300

    change code

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -15,26 +15,22 @@ Student.prototype.setSubject = function (subjectName) {
 }
  
 Student.prototype.addMarks = function (...marksToAdd) {
-  if(this.marks === undefined) {
-    return 0;
-  } else {
-    this.marks.push(...marksToAdd);
+  if(this.marks !== undefined) {
+     this.marks.push(...marksToAdd);
   }
 }
  
 Student.prototype.getAverage = function(){
-    if(this.marks !== undefined) {
-    return this.marks.reduce((a, b) => a + b, 0) / this.marks.length || 0;
-  } else if (this.marks === undefined || 0) {
-      return this.marks = 0;
-}
+  if(this.marks !== undefined) {
+      return this.marks.reduce((a, b) => a + b, 0) / this.marks.length || 0;
+  } else {
+    return 0;
+  }
 }
 
 Student.prototype.exclude = function(reason){
-    delete this.subject;
-    delete this.marks;
-    this.exclude = reason;
-
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 }
-
 
